20ce2705a56244679e47323ec1ff8cd2e37c4046
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-11-25 00:58:50 +0100

    Update z01.cpp

--- v01/z01/z01.cpp
+++ v01/z01/z01.cpp
@@ -14,56 +14,56 @@
 
 int main()
 {
-	// 1. Inicijalizovati windosk biblioteku
+	// 1. Inicijalizovati winsock biblioteku
 	WSADATA wsaData;
-	int rezultat_inicijalizacije = WSAStartup(MAKEWORD(2,2), &wsaData);
-	if (rezultat_inicijalizacije != NO_ERROR)
+	int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (iResult != NO_ERROR)
 	{
-		printf("Greska pri pozivu WSAStartup().\n");
+		printf("Greska pri inicijalizaciji winsock biblioteke.\n");
 		return 1;
 	}
 	else
 	{
-		printf("WSA biblioteka je uspesno incijalizovana.\n");
+		printf("Bibilioteka uspesno pokrenuta.\n");
 	}
 
 	// 2. Kreirati adresnu strukturu koja koristi 
 	// ip adresu 127.0.0.1 i broj porta 5555
-	unsigned short port = 5555;
-	const char* ip = "127.0.0.1";
+	int port = 5555;
+	char* ip = "127.0.0.1";
 
-	sockaddr_in adresaServera;
+	sockaddr_in = adresaServera;
 	adresaServera.sin_family = AF_INET;
-	adresaServera.sin_addr.s_addr = inet_addr(ip);
 	adresaServera.sin_port = htons(port);
+	adresaServera.sin_addr.s_addr = inet_addr(ip);
 
 	// 3. Kreirati jedan TCP soket (utičnicu)
 	SOCKET serverSoket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (serverSoket == INVALID_SOCKET)
 	{
-		printf("Greska pri pozivu socket().\n");
+		printf("Greska pri pravljenju uticnice za server.\n");
 		WSACleanup();
 		return 1;
 	}
 	else
 	{
-		printf("\nTCP soket je uspesno napravljen.\n");
+		printf("Uticnica za server je uspesno napravljena.\n");
 	}
-
+	
 	// 4. Povezati soket sa kreiranom adresnom strukturom
-	if (bind(serverSoket, (SOCKADDR*) &adresaServera, sizeof(adresaServera)) == SOCKET_ERROR)
+	if (bind(serverSocket, (SOCKADDR*) &adresaServera, sizeof(adresaServera)) == SOCKET_ERROR)
 	{
 		int kodGreske = WSAGetLastError();
-		printf("Neuspesan poziv funkcije bind().\n");
-		printf("ERROR: %d\n", kodGreske);
+		printf("Neuspesno spajanje uticnice sa adresom.\n");
+		printf("GRESKA: %d\n", kodGreske);
 
-		closesocket(serverSoket);
+		closesocket(serverSocket);
 		WSACleanup();
-		return 0;
+		return 1;
 	}
 	else
 	{
-		printf("\nSoket je uspesno spojen sa adresom.\n");
+		printf("Uticnica je uspesno spojena sa adresom.\n");
 	}
 
 	// 5. Zatvoriti soket

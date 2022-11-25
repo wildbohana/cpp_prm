b385c4ffdd977c2c07b0bd2700f9c5a67cbe617e
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-11-25 01:04:53 +0100

    Update Server.cpp

--- v04/z02/Server.cpp
+++ v04/z02/Server.cpp
@@ -1,4 +1,4 @@
-// UDP server that use blocking sockets
+// UDP server, blokirajuce uticnice
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 
 #define WIN32_LEAN_AND_MEAN
@@ -14,106 +14,87 @@
 #pragma comment (lib, "Mswsock.lib")
 #pragma comment (lib, "AdvApi32.lib")
 
-// DVE SERVERSKE UTIČNICE UMESTO JEDNE
 #define SERVER_PORT1 17010 
 #define SERVER_PORT2 17011
 
-// Size of buffer that will be used for sending and receiving messages to clients
 #define BUFFER_SIZE 512		
 
 int main()
 {
     // Dve adresne strukture za svaku serversku uticnicu
-    sockaddr_in serverAddress1, serverAddress2;
+    sockaddr_in adresaServera1, adresaServera2;
 
-    // Buffer we will use to send and receive clients' messages
     char dataBuffer[BUFFER_SIZE];
 
-    // WSADATA data structure that is to receive details of the Windows Sockets implementation
+	int iResult;
+
     WSADATA wsaData;
 
-    // Initialize windows sockets library for this process
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
     {
-        printf("WSAStartup neuspesan.\nGreska: %d\n", WSAGetLastError());
+		printf("Pokretanje winsock biblioteke neuspesno.\nGRESKA: %d\n", WSAGetLastError());
         return 1;
     }
 
-	// Popunjavanje adresnih struktura, kreiranje dve uticnice i
-	// njihovo povezivanje sa adresnim podacima u tim strukturama
+    memset((char*) &adresaServera1, 0, sizeof(adresaServera1));
+    adresaServera1.sin_family = AF_INET; 
+    adresaServera1.sin_addr.s_addr = inet_addr("127.0.0.1");
+    adresaServera1.sin_port = htons(SERVER_PORT1);		// 17010 port
 
-    // Initialize serverAddress1 and serverAddress2 structures used by bind function
-    memset((char*) &serverAddress1, 0, sizeof(serverAddress1));
-    serverAddress1.sin_family = AF_INET; 
-    serverAddress1.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serverAddress1.sin_port = htons(SERVER_PORT1);	// 17010 port
+    memset((char*) &adresaServera2, 0, sizeof(adresaServera2));
+    adresaServera2.sin_family = AF_INET;
+    adresaServera2.sin_addr.s_addr = INADDR_ANY;
+    adresaServera2.sin_port = htons(SERVER_PORT2);		// 17011 port
 
-    memset((char*) &serverAddress2, 0, sizeof(serverAddress2));
-    serverAddress2.sin_family = AF_INET;
-    serverAddress2.sin_addr.s_addr = INADDR_ANY;
-    serverAddress2.sin_port = htons(SERVER_PORT2);	// 17011 port
-
-    // Create first UDP socket
-    SOCKET serverSoket1 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
-    // Check if socket creation succeeded
-    if (serverSoket1 == INVALID_SOCKET)
+    SOCKET serverskaUticnica1 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (serverskaUticnica1 == INVALID_SOCKET)
     {
-        printf("Neuspesno stvaranje soketa.\nGreska: %d\n", WSAGetLastError());
+		printf("Otvaranje serverske uticnice neuspesno.\nGRESKA: %d\n", WSAGetLastError());
         WSACleanup();
         return 1;
     }
 
-    // Create second UDP socket
-    SOCKET serverSoket2 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
-    // Check if socket creation succeeded
-    if (serverSoket2 == INVALID_SOCKET)
+    SOCKET serverskaUticnica2 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (serverskaUticnica2 == INVALID_SOCKET)
     {
-        printf("Neuspesno stvaranje soketa.\nGreska: %d\n", WSAGetLastError());
+		printf("Otvaranje serverske uticnice neuspesno.\nGRESKA: %d\n", WSAGetLastError());
         WSACleanup();
         return 1;
     }
 
     // Povezujemo prvu uticnicu sa njenom adresnom strukturom 
-    int iResult = bind(serverSoket1, (SOCKADDR*) &serverAddress1, sizeof(serverAddress1));
-
-    // Check if socket is succesfully binded to server datas
+    iResult = bind(serverskaUticnica1, (SOCKADDR*) &adresaServera1, sizeof(adresaServera1));
     if (iResult == SOCKET_ERROR)
     {
-        printf("bind uticnice neuspesan.\nGreska: %d\n", WSAGetLastError());
-        closesocket(serverSoket1);
+		printf("Povezivanje serverske uticnice sa adresom neuspesno.\nGRESKA: %d\n", WSAGetLastError());
+        closesocket(serverskaUticnica1);
         WSACleanup();
         return 1;
     }
 
     // Povezujemo drugu uticnicu sa njenom adresnom strukturom 
-    iResult = bind(serverSoket2, (SOCKADDR*) &serverAddress2, sizeof(serverAddress2));
-
-    // Check if socket is succesfully binded to server address data
+    iResult = bind(serverskaUticnica2, (SOCKADDR*) &adresaServera2, sizeof(adresaServera2));
     if (iResult == SOCKET_ERROR)
     {
         printf("bind uticnice neuspesan.\nGreska: %d\n", WSAGetLastError());
-        closesocket(serverSoket2);
+        closesocket(serverskaUticnica2);
         WSACleanup();
         return 1;
     }
 
 	printf("UDP server je pokrenut i ceka na poruke od klijenata.\n");
 
-    // Declare client address that will be set from recvfrom
     sockaddr_in adresaKlijenta;
-    // size of client address structure
     int duzinaAdreseKlijenta = sizeof(adresaKlijenta);
 
 	// Stavljamo uticnice u neblokirajuci rezim (mode = 1)
     unsigned long mode = 1;
-    if (ioctlsocket(serverSoket1, FIONBIO, &mode) != 0 
-	||  ioctlsocket(serverSoket2, FIONBIO, &mode) != 0)
+    if (ioctlsocket(serverskaUticnica1, FIONBIO, &mode) != 0 
+	||  ioctlsocket(serverskaUticnica2, FIONBIO, &mode) != 0)
     {
-        printf("ioctlsocket neuspesan.\nGreska: %d\n", WSAGetLastError());
-        closesocket(serverSoket1);
-        closesocket(serverSoket2);
+		printf("Prebacivanje servera u neblokirajuci rezim neuspesno.\nGRESKA: %d\n", WSAGetLastError());
+        closesocket(serverskaUticnica1);
+        closesocket(serverskaUticnica2);
         WSACleanup();
         return 1;
     }
@@ -123,95 +104,59 @@ int main()
     int poeni1 = poeni2 = 0; 
 
 	// Polling model prijema poruka na ove dve uticnice
-    while (1)
+    while (true)
     {
-        // Initialize client address structure in memory
         memset(&adresaKlijenta, 0, sizeof(adresaKlijenta));
 
-        // Set whole buffer to zero
         memset(dataBuffer, 0, BUFFER_SIZE);
 
         // Prijem poruke sa prve uticnice
-        iResult = recvfrom
-			(serverSoket1,				// Own socket
-            dataBuffer,					// Buffer that will be used for receiving message
-            BUFFER_SIZE,				// Maximal size of buffer
-            0,							// No flags
-            (SOCKADDR*)&adresaKlijenta,	// Client information from received message (ip address and port)
-            &duzinaAdreseKlijenta);		// Size of sockadd_in structure
-
-        // Provera da li je uspesno primljena poruka
-        if (iResult != SOCKET_ERROR)
+    	iResult = recvfrom(serverskaUticnica1, dataBuffer, BUFFER_SIZE, 0, (SOCKADDR*) &adresaKlijenta, &duzinaAdreseKlijenta);
+	    if (iResult != SOCKET_ERROR)
         {
-            // Set end of string
             dataBuffer[iResult] = '\0';
-
-            // Ispis primljene poruke na ekran
-            printf("Serverska uticnica br. 1 je primila: %s.\n", dataBuffer);
+            printf("Serverska uticnica 1 je primila: %s.\n", dataBuffer);
 			
-			// Prvi klijent je poslao poruku i ona je primljena
             primljen1 = true; 
-            
-			// Ako je stigla poruka od 1. klijenta, ali ne i od 2. klijenta
+
             if (primljen1 && !primljen2)
             {
                 poeni1++;
                 printf("Prvi klijent je prvi poslao poruku i ima %d poena.\n", poeni1);
             }
         }
-		// iResult == SOCKET_ERROR tj. desila se greska
         else  
         {
             // Obavezno proveriti da li je operacija vratila WSAEWOULDBLOCK
-            // To oznacava da bi operacija recvfrom() blokirala programa
-			// jer jos nije spremna da se izvrsi tj. poruka nije stigla
             if (WSAGetLastError() == WSAEWOULDBLOCK) 
 			{
-                // Pusticemo da program tece dalje jer imamo pokusaj 
-                // prijema poruke i na 2. uticnici, tamo cemo pozvati Sleep
+                // Pusticemo da program tece dalje u uticnici 1, ali cemo u uticnici 2 pozvari sleep
             }
             else 
 			{
-                // Desila se neka druga greska prilikom prijema poruke
-                // izaci cemo iz petlje, zatvoriti uticnice i ugasiti program
+                // Desila se neka druga greska prilikom prijema poruke, gasimo ceo server (zato ide break)
                 break;
             }
         }
 
         // Prijem poruke sa druge uticnice
-        iResult = recvfrom
-			(serverSoket2,					// Own socket
-            dataBuffer,						// Buffer that will be used for receiving message
-            BUFFER_SIZE,					// Maximal size of buffer
-            0,								// No flags
-            (SOCKADDR*) &adresaKlijenta,	// Client information from received message (ip address and port)
-            &duzinaAdreseKlijenta);			// Size of sockadd_in structure
-
-        // Provera da li je uspesno primljena poruka
+        iResult = recvfrom(serverskaUticnica2, dazaBuffer, BUFFER_SIZE, 0, (SOCKADDR*) &adresaKlijenta, duzinaAdreseKlijenta);
         if (iResult != SOCKET_ERROR)
         {
-            // Set end of string
             dataBuffer[iResult] = '\0';
-
-            // Ispis primljene poruke na ekran
-            printf("Serverska uticnica br. 2 je primila: %s.\n", dataBuffer);
+            printf("Serverska uticnica 2 je primila: %s.\n", dataBuffer);
 			
-			// Drugi klijent je poslao poruku i ona je primljena
             primljen2 = true; 
             
-			// Ako je stigla poruka od 2. klijenta, ali ne i od 1. klijenta
             if (!primljen1 && primljen2) 
             {
                 poeni2++;
                 printf("Drugi klijent je prvi poslao poruku i ima %d poena.\n", poeni2);
             }
         }
-		// iResult == SOCKET_ERROR tj. desila se greska
         else  
         {
             // Obavezno proveriti da li je operacija vratila WSAEWOULDBLOCK
-            // To oznacava da bi operacija recvfrom() blokirala programa
-            // jer jos nije spremna da se izvrsi tj. poruka nije stigla
             if (WSAGetLastError() == WSAEWOULDBLOCK) 
 			{
 				// Cekamo do narednog pokusaja prijema poruke 1.5s
@@ -219,8 +164,7 @@ int main()
             }
             else 
 			{
-                // Desila se neka druga greska prilikom prijema poruke
-                // Izaci cemo iz petlje, zatvoriti uticnice i ugasiti program
+                // Desila se neka druga greska prilikom prijema poruke, gasimo server
                 break;
             }
         }
@@ -233,26 +177,26 @@ int main()
         }
     }
 
-    // Close server application - za oba soketa
-    iResult = closesocket(serverSoket1);
+	// Zatvaramo prvu uticnicu
+    iResult = closesocket(serverskaUticnica1);
     if (iResult == SOCKET_ERROR)
     {
-        printf("closesocket neuspesan.\nGreska: %ld\n", WSAGetLastError());
+		printf("Zatvaranje serverske uticnice neuspesno.\nGRESKA: %d\n", WSAGetLastError());
         WSACleanup();
         return 1;
     }
 
-    iResult = closesocket(serverSoket2);
+	// Zatvaramo drugu uticnicu
+    iResult = closesocket(serverskaUticnica2);
     if (iResult == SOCKET_ERROR)
     {
-        printf("closesocket neuspesan.\nGreska: %ld\n", WSAGetLastError());
+		printf("Zatvaranje serverske uticnice neuspesno.\nGRESKA: %d\n", WSAGetLastError());
         WSACleanup();
         return 1;
     }
 
     printf("Server je uspesno ugasen.\n");
 
-    // Close Winsock library
     WSACleanup();
 
     return 0;
